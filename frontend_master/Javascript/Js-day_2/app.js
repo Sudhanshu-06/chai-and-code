76619c48e2eb634350ba627f499ab42068816cf5
Sudhanshu-06 <sudhanshu767778@gmail.com>
2025-03-04 13:57:57 +0530

    done

---
+++ frontend_master/Javascript/Js-day_2/app.js
@@ -0,0 +1,19 @@
+// Static types- c, c++, java
+
+// Dynamic types- JavaScript
+
+let age=9;
+console.log(typeof age);
+
+// type conversions
+console.log("4"-2);
+console.log("4"+2);
+console.log(4-"2");
+console.log(4+"2");
+
+num="8"
+let rsult= 4+num++;
+console.log(rsult);
+
+let num1= 23;
+console.log(typeof num1, num1.toString());
